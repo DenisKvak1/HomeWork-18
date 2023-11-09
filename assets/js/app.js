c16d6f4e997b706362abf4373a4d56fb8618031d
DenisKvak1 <denis.kvak457@gmail.com>
2023-11-09 18:40:42 +0200

    4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,13 @@
-
-let Calculate={
-    SquarePerimeter:(side)=>typeof(side)=='number' ? (side * 4).toFixed(2) : NaN,
-    CubeVolume:(edge)=>typeof(edge)=='number' ? (Math.pow(edge, 3)).toFixed(2) : NaN,
-    CubeSideSurfaceArea: (edge) => (typeof(edge) == 'number' ? (4 * Math.pow(edge, 2)).toFixed(2) : NaN),
-    СircumFerence: (radius) => (typeof(radius) == 'number' ? (2*radius*Math.PI).toFixed(2) : NaN),
-    AreaOfCircle: (radius) => (typeof(radius) == 'number' ? (Math.PI * Math.pow(radius, 2)).toFixed(2) : NaN),
-    MaterialDensity: (volume, mass) => (typeof(volume) == 'number' && typeof(mass) == 'number' && mass > 0 ? (mass / volume).toFixed(2) : NaN),
-    PopulationDensity: (population, area) => (typeof(population) == 'number' && typeof(area) == 'number' && area > 0 ? (population / area).toFixed(2) : NaN),
-    Hypotenuse: (a, b) => (typeof(a) == 'number' && typeof(b) == 'number' && a > 0 && b > 0 ? Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)).toFixed(2) : NaN),
-    CalculateY: (a) => (typeof(a) == 'number' ? (Math.pow(a, 2) + 10 / Math.sqrt(Math.pow(a, 2) + 1)).toFixed(2) :  NaN),
+let Calculate = {
+    SquarePerimeter: (side) => typeof (side) == 'number' ? (side * 4).toFixed(2) : NaN,
+    CubeVolume: (edge) => typeof (edge) == 'number' ? (Math.pow(edge, 3)).toFixed(2) : NaN,
+    CubeSideSurfaceArea: (edge) => (typeof (edge) == 'number' ? (4 * Math.pow(edge, 2)).toFixed(2) : NaN),
+    СircumFerence: (radius) => (typeof (radius) == 'number' ? (2 * radius * Math.PI).toFixed(2) : NaN),
+    AreaOfCircle: (radius) => (typeof (radius) == 'number' ? (Math.PI * Math.pow(radius, 2)).toFixed(2) : NaN),
+    MaterialDensity: (volume, mass) => (typeof (volume) == 'number' && typeof (mass) == 'number' && mass > 0 ? (mass / volume).toFixed(2) : NaN),
+    PopulationDensity: (population, area) => (typeof (population) == 'number' && typeof (area) == 'number' && area > 0 ? (population / area).toFixed(2) : NaN),
+    Hypotenuse: (a, b) => (typeof (a) == 'number' && typeof (b) == 'number' && a > 0 && b > 0 ? Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)).toFixed(2) : NaN),
+    CalculateY: (a) => (typeof (a) == 'number' ? (Math.pow(a, 2) + 10 / Math.sqrt(Math.pow(a, 2) + 1)).toFixed(2) : NaN),
     Descriptions: {
         SquarePerimeter: "Периметр Квадрата",
         CubeVolume: "Объем Куба",
@@ -20,21 +19,36 @@ let Calculate={
         Hypotenuse: "Гипотенуза прямоугольного треугольника",
         CalculateY: "Вычислить значение Y",
     },
+    InputPlaceholders: {
+        SquarePerimeter: ["Введите длину стороны квадрата"],
+        CubeVolume: ["Введите длину ребра куба"],
+        CubeSideSurfaceArea: ["Введите длину ребра куба"],
+        СircumFerence: ["Введите радиус круга"],
+        AreaOfCircle: ["Введите радиус круга"],
+        MaterialDensity: ["Введите объем материала", "Введите массу материала"],
+        PopulationDensity: ["Введите население", "Введите площадь"],
+        Hypotenuse: ["Введите длину первого катета", "Введите длину второго катета"],
+        CalculateY: ["Введите значение a для вычисления Y"],
+    },
 }
+
 function updateInputsForFunction(func, containerId) {
     const inputContainer = document.getElementById(containerId);
     const argNames = func.toString().match(/\(([^)]+)\)/)[1].split(',').map(arg => arg.trim());
 
-    inputContainer.innerHTML = ''; 
-    argNames.forEach(argName => {
+    inputContainer.innerHTML = '';
+    argNames.forEach((argName, index) => {
+        const selectedMethod = inputMethod.value;
         const input = document.createElement('input');
         inputContainer.appendChild(input);
-        input.classList.add('form-control','w-25','mt-2');
+        input.classList.add('form-control', 'w-25', 'mt-2');
+        input.placeholder = (Calculate.InputPlaceholders[selectedMethod] && Calculate.InputPlaceholders[selectedMethod][index]) || "Введите значение";
     });
 }
+
 function populateSelectOptions(optionsArray, selectId) {
     const selectElement = document.getElementById(selectId);
-    let o=0;
+    let o = 0;
     optionsArray.forEach(optionValue => {
         const optionElement = document.createElement('option');
 
@@ -45,24 +59,30 @@ function populateSelectOptions(optionsArray, selectId) {
         o++;
     });
 }
+
 let button = document.querySelector('#calcB');
 let result = document.querySelector('#result');
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
     const selectMethod = inputMethod.value;
     const inputContainer = document.getElementById('input-block');
     const inputElements = inputContainer.querySelectorAll('input');
     const argsArray = Array.from(inputElements).map(input => parseFloat(input.value.trim()));
-    result.textContent = `Результат ${selectMethod}: ${Calculate[selectMethod](...argsArray)}`;
+    if(isNaN(Calculate[selectMethod](...argsArray))){
+        result.textContent = `Введите корректные данные :)`;
+    }
+    else{
+        result.textContent = `Результат: ${Calculate[selectMethod](...argsArray)}`;
+    }
 });
+
 let inputMethod = document.querySelector('#formul');
-inputMethod.addEventListener('change', function() {
+inputMethod.addEventListener('change', function () {
     const selectedMethod = inputMethod.value;
     if (Calculate[selectedMethod]) {
         updateInputsForFunction(Calculate[selectedMethod], 'input-block');
     }
 });
-const methodNames = Object.keys(Calculate).filter(key => key !== 'Descriptions');
+
+const methodNames = Object.keys(Calculate).filter(key => key !== 'Descriptions' && key !== 'InputPlaceholders');
 populateSelectOptions(methodNames, 'formul');
 updateInputsForFunction(Calculate.SquarePerimeter, 'input-block');
-
-
