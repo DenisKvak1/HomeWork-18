3b7541e9b17f260c159dce088c55b68b84490e0a
DenisKvak1 <denis.kvak457@gmail.com>
2023-11-09 18:01:45 +0200

    2

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,9 +9,42 @@ let Calculate={
     PopulationDensity: (population, area) => (typeof(population) == 'number' && typeof(area) == 'number' && area > 0 ? (population / area).toFixed(2) : NaN),
     Hypotenuse: (a, b) => (typeof(a) == 'number' && typeof(b) == 'number' && a > 0 && b > 0 ? Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)).toFixed(2) : NaN),
     CalculateY: (a) => (typeof(a) == 'number' ? (Math.pow(a, 2) + 10 / Math.sqrt(Math.pow(a, 2) + 1)).toFixed(2) :  NaN),
+    Descriptions: {
+        SquarePerimeter: "Calculate the perimeter of a square",
+        CubeVolume: "Calculate the volume of a cube",
+        CubeSideSurfaceArea: "Calculate the surface area of a cube",
+        СircumFerence: "Calculate the circumference of a circle",
+        AreaOfCircle: "Calculate the area of a circle",
+        MaterialDensity: "Calculate material density",
+        PopulationDensity: "Calculate population density",
+        Hypotenuse: "Calculate the hypotenuse of a right triangle",
+        CalculateY: "Calculate Y value",
+    },
 }
-let inputMethod = document.querySelector('#inputMethod');
-let inputValue = document.querySelector('#inputValue');
+function updateInputsForFunction(func, containerId) {
+    const inputContainer = document.getElementById(containerId);
+    const argNames = func.toString().match(/\(([^)]+)\)/)[1].split(',').map(arg => arg.trim());
+
+    inputContainer.innerHTML = ''; 
+    argNames.forEach(argName => {
+        const input = document.createElement('input');
+        inputContainer.appendChild(input);
+        input.classList.add('form-control','w-25','mt-2');
+    });
+}
+function populateSelectOptions(optionsArray, selectId) {
+    const selectElement = document.getElementById(selectId);
+    let o=0;
+    optionsArray.forEach(optionValue => {
+        const optionElement = document.createElement('option');
+
+        optionElement.value = o + 'select';
+
+        optionElement.textContent = Calculate.Descriptions[optionValue];
+        selectElement.appendChild(optionElement);
+        o++;
+    });
+}
 let button = document.querySelector('#calcB');
 let result = document.querySelector('#result');
 button.addEventListener("click", function() {
@@ -38,12 +71,16 @@ button.addEventListener("click", function() {
         }
     }
 });
+let inputMethod = document.querySelector('#formul');
+inputMethod.addEventListener('change', function() {
+    const selectedDescription = inputMethod.textContent;
+    const selectedMethod = Object.keys(Calculate.Descriptions).find(key => Calculate.Descriptions[key] === selectedDescription);
+    if (Calculate[selectedMethod]) {
+        updateInputsForFunction(Calculate[selectedMethod], 'input-block');
+    }
+});
+const methodNames = Object.keys(Calculate).filter(key => key !== 'Descriptions');
+populateSelectOptions(methodNames, 'formul');
+updateInputsForFunction(Calculate.SquarePerimeter, 'input-block');
 
 
-let methodNames = Object.keys(Calculate);
-let pElement = document.getElementById("p");
-if (methodNames.length > 0) {
-    pElement.innerHTML = methodNames.join("<br>");
-} else {
-    pElement.textContent = "Нет доступных методов.";
-}
